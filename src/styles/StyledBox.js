f17ad8ad34fa5b55aa65dcd67c6ce449f65fa237
usernamebuzz <ohminji@ewhain.net>
2022-06-21 13:39:22 +0900

    Chore: Increase padding

--- src/styles/StyledBox.js
+++ src/styles/StyledBox.js
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
 
 export const StyledBox = styled.section`
-  padding: 15px;
+  padding: 20px;
   margin: 15px;
   cursor: pointer;
   display: flex;
   flex-direction: column;
   background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(4px);
   -webkit-backdrop-filter: blur(4px);
   border-radius: 10px;
--- src/styles/StyledButton.js
+++ src/styles/StyledButton.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.section`
-  padding: 15px;
+  padding: 20px;
   margin: 15px;
   cursor: pointer;
   display: flex;
   flex-direction: column;
   background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(4px);
-  -webkit-backdrop-filter: blur(4px);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
   border-radius: 10px;
   border: 1px solid rgba(255, 255, 255, 0.18);
   width: 10%;
