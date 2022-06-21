4d632f5323dbc3ed87e1ca57d77af8b5acb651e6
YoommY <seeun0792@gmail.com>
2022-06-22 03:10:30 +0900

    feat : 결과화면 구성 완료, style : 찔끔

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import { Navigate, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { API } from "../API/API";
-
+import styled from "styled-components";
+import { CenteringWrapper ,Header } from "../GlobalStyle";
+import { StyledButton } from "../styles/StyledButton";
 function SignUpPage(){
 
     const navigate = useNavigate();
@@ -70,37 +72,73 @@ function SignUpPage(){
 
     return(
         <>
-        <div>회원가입 페이지</div>
-        <form onSubmit={signUpDB}>
-        <input
+        <Header>회원가입 페이지</Header>
+        <CenteringWrapper>
+        <Form onSubmit={signUpDB}>
+        <Text>아이디</Text>
+        <Input
         type="id"
         name="id"
         value={inputText.id}
         onChange={onInputChange}
         spellCheck="false"
         />
-        <input
+        <Text>이메일</Text>
+        <Input
         type="email"
         name="email"
         value={inputText.email}
         onChange={onInputChange}
         spellCheck="false"
         />
-        <input
+        <Text>비밀번호</Text>
+        <Input
         type="pw"
         name="pw"
         value={inputText.pw}
         onChange={onInputChange}
         spellCheck="false"
         />
-        <button>회원가입</button>
-        </form>
+        <StyledButton>회원가입</StyledButton>
         <div>이미 회원이신가요?
             <Link to={`/LoginPage`}>로그인</Link>
         </div>
+        </Form>
+        
+        </CenteringWrapper>
         </>
     )
 }
+
+const Form = styled.form`
+
+background-color: rgba(255, 255, 255, 0.25);
+height:100%;
+width:50%;
+padding:20px;
+border-radius: 10px;
+border: 1px solid rgba(255, 255, 255, 0.18);
+
+
+align-items: center;
+justify-content: center;
+display:flex;
+flex-direction: column;
+
+`
+const Input = styled.input`
+
+height:50px;
+width:300px;
+margin:10px;
+
+
+`
+const Text = styled.div`
+font-size:15px;
+
+`
+
 
 export default SignUpPage;
 
