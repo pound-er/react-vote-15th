bb675f2e3f60207f55b184aa577ad7c65c032f87
usernamebuzz <ohminji@ewhain.net>
2022-06-21 18:18:10 +0900

    Bug: Axios post 500

--- src/pages/VotePage.js
+++ src/pages/VotePage.js
@@ -24,6 +24,40 @@ function VotePage() {
     fetcthCandidates();
   }, []);
 
+  const handleVote = (index) => {
+    const formData = candidates[index].name;
+
+    const headers = {
+      'Content-type': 'multipart/form-data',
+    };
+    axios.defaults.headers.post = null;
+
+    axios
+      .post(
+        'http://ec2-3-38-228-115.ap-northeast-2.compute.amazonaws.com/api/vote/',
+        {
+          candidate: formData,
+        },
+        {
+          headers,
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        setCandidates((candidates) =>
+          candidates.map((item) => {
+            if (item.id === index) {
+              return { ...item, vote_ctn: item.vote_ctn + 1 };
+            }
+            return item;
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   if (!candidates) return null;
   return (
     <>
@@ -35,7 +69,7 @@ function VotePage() {
       </Link>
       <CenteringWrapper>
         {candidates.map((user) => (
-          <StyledBox key={user.id}>
+          <StyledBox key={user.id} onClick={() => handleVote(user.id - 1)}>
             {user.candidate_name} ({user.description})
           </StyledBox>
         ))}
